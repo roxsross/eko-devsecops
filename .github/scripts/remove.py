34bf0a9a9eea749a0b812beb2d12e6c875ba4f5a
Rossana Suarez <46635001+roxsross@users.noreply.github.com>
2024-11-02 13:40:20 -0300

    e

--- .github/scripts/remove.py
+++ .github/scripts/remove.py
@@ -1,19 +1,19 @@
 import json
 import sys
 
-def remove_duplicates(sarif_file):
-    with open(sarif_file, 'r') as file:
+def remove_duplicates(input_file, output_file):
+    with open(input_file, 'r') as file:
         data = json.load(file)
 
     rules = data['runs'][0]['tool']['driver']['rules']
     unique_rules = {rule['id']: rule for rule in rules}.values()
     data['runs'][0]['tool']['driver']['rules'] = list(unique_rules)
 
-    with open(sarif_file, 'w') as file:
+    with open(output_file, 'w') as file:
         json.dump(data, file, indent=2)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python remove.py <sarif_file>")
+    if len(sys.argv) != 3:
+        print("Usage: python remove.py <input_sarif_file> <output_sarif_file>")
         sys.exit(1)
-    remove_duplicates(sys.argv[1])
+    remove_duplicates(sys.argv[1], sys.argv[2])
